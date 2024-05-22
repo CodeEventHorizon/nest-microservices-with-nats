c6da1987e8ee81999502c1816b5767e3d2fa093a
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-22 22:56:51 +0400

    update app module

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'NATS_SERVICE',
-        transport: Transport.NATS,
-        options: {
-          // hostname is this way, as its run in docker
-          // In case of network it would be nats://10.11.12.13:4222
-          servers: ['nats://nats'],
-        },
-      },
-    ]),
-  ],
+  imports: [UsersModule],
   controllers: [],
   providers: [],
 })
