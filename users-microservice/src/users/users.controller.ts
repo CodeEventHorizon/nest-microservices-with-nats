d0b481986586aae6d1701399e769997e93716ed1
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-23 01:14:38 +0400

    add user microservice controller

---
+++ users-microservice/src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateUserDto } from './dtos/CreateUser.dto';
+
+@Controller()
+export class UsersMicroserviceController {
+  @MessagePattern({ cmd: 'createUser' })
+  createUser(@Payload() data: CreateUserDto) {
+    console.log(data);
+    return data;
+  }
+}
