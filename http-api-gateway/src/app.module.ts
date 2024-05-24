e5859fe30df80f9fcc77ff8530c896f5e5ccf5b3
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-24 23:06:00 +0400

    add payments module

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
