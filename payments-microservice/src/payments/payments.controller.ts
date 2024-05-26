e89f4f103196b4b2e3fa5a970a1f4ba5634fd6d4
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-26 22:32:54 +0400

    inject nats client into payments controller

--- payments-microservice/src/payments/payments.controller.ts
+++ payments-microservice/src/payments/payments.controller.ts
@@ -1,9 +1,10 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices';
 import { CreatePaymentDto } from './dtos/CreatePayment.dto';
 
 @Controller()
 export class PaymenentsMicroserviceController {
+  constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
   @EventPattern('createPayment')
   createPayment(@Payload() createPaymentDto: CreatePaymentDto) {
     console.log(createPaymentDto);
