cfc238dc8e324fcaddcc762b7231333af72cd310
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-26 22:30:33 +0400

    add nats client to payments microservice

--- payments-microservice/src/app.module.ts
+++ payments-microservice/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { NatsClientModule } from './nats-client/nats-client.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, NatsClientModule],
   controllers: [],
   providers: [],
 })
