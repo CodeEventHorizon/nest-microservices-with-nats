f145413a77a90f85896bc0b77ccd4212afdca4b7
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-28 03:19:18 +0400

    add getUserById controller

--- users-microservice/src/users/users.controller.ts
+++ users-microservice/src/users/users.controller.ts
@@ -11,6 +11,12 @@ export class UsersMicroserviceController {
     return this.usersService.createUser(data);
   }
 
+  @MessagePattern({ cmd: 'getUserById' })
+  async getUserById(@Payload() data: any) {
+    const { userId } = data;
+    return await this.usersService.getUserById(userId);
+  }
+
   @EventPattern('paymentCreated')
   paymentCreated(@Payload() data: any) {
     console.log(data);
