ca565277422b9c15f1e858f3b53dc4fb90e9911a
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-27 04:41:06 +0400

    update users controller with users service

--- users-microservice/src/users/users.controller.ts
+++ users-microservice/src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateUserDto } from './dtos/CreateUser.dto';
+import { UsersService } from './users.service';
 
 @Controller()
 export class UsersMicroserviceController {
+  constructor(private usersService: UsersService) {}
   @MessagePattern({ cmd: 'createUser' })
   createUser(@Payload() data: CreateUserDto) {
-    console.log(data);
-    return { msg: 'Success' };
+    return this.usersService.createUser(data);
   }
 
   @EventPattern('paymentCreated')
