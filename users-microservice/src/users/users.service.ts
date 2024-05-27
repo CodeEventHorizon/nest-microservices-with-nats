b7192f64e321634bbc07b343cd9917410f41d30e
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-27 04:40:45 +0400

    add users service

---
+++ users-microservice/src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/CreateUser.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
+  }
+}
