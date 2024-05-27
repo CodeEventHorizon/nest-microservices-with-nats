378e214548641ef68626019271ce63c12b3fd7c2
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-27 04:16:03 +0400

    add user table entity

---
+++ users-microservice/src/typeorm/entities/User.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  username: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: true })
+  displayName?: string;
+}
