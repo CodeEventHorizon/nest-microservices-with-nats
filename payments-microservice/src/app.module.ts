8688885a6e22fc00d18dccc7cfcd4763196460c4
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-28 00:58:27 +0400

    update app module

--- payments-microservice/src/app.module.ts
+++ payments-microservice/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './typeorm/entities/Payment';
 // import { NatsClientModule } from './nats-client/nats-client.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nestjs_db',
+      entities: [Payment],
+      synchronize: true,
+      username: 'testuser',
+      password: 'testuser123',
+    }),
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
