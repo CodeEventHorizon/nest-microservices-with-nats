18005e0b0dc84ae7480318b1c9298327a41a2a5a
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-28 03:19:58 +0400

    add createPayment service

--- payments-microservice/src/payments/payments.service.ts
+++ payments-microservice/src/payments/payments.service.ts
@@ -1,17 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Payment } from 'src/typeorm/entities/Payment';
 import { Repository } from 'typeorm';
 import { CreatePaymentDto } from './dtos/CreatePayment.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { User } from 'src/typeorm/entities/User';
 
 @Injectable()
 export class PaymentsService {
   constructor(
     @InjectRepository(Payment) private paymentsRepository: Repository<Payment>,
+    @Inject('NATS_SERVICE') private natsClient: ClientProxy,
   ) {}
 
-  createPayment(createPaymentDto: CreatePaymentDto) {
-    const newPayment = this.paymentsRepository.create(createPaymentDto);
-    return this.paymentsRepository.save(newPayment);
+  async createPayment({ userId, ...createPaymentDto }: CreatePaymentDto) {
+    const user = await lastValueFrom<User>(
+      this.natsClient.send({ cmd: 'getUserById' }, { userId }),
+    );
+
+    console.log(user);
+
+    if (user) {
+      const newPayment = this.paymentsRepository.create({
+        ...createPaymentDto,
+        user,
+      });
+
+      console.log(newPayment);
+
+      return this.paymentsRepository.save(newPayment);
+    }
+
+    return null;
   }
 }
