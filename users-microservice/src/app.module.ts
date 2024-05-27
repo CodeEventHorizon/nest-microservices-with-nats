95781ec755f7b0799afc89ec03b8019f3f5fcf6d
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-27 04:16:15 +0400

    add typeorm module

--- users-microservice/src/app.module.ts
+++ users-microservice/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nestjs_db',
+      entities: [User],
+      synchronize: true,
+      username: 'testuser',
+      password: 'testuser123',
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
