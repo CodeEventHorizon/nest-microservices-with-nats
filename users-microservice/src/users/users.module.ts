f7c137e655656b6dcb8e0f38850c73d25314710c
Nikoloz Muladze <nikoloz.9374@gmail.com>
2024-05-27 04:41:21 +0400

    update users module

--- users-microservice/src/users/users.module.ts
+++ users-microservice/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersMicroserviceController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersMicroserviceController],
-  providers: [],
+  providers: [UsersService],
 })
 export class UsersModule {}
